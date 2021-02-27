ec635437d76ee53f0664b77fb09a1a5ebc700819
AJ Green <aj@greenvariable.com>
2021-02-26 23:25:17 -0500

    Removed contact popup and jspopup dependnecy. Going mailto route.

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,29 +1,16 @@
 import React from 'react';
-import Popup from 'reactjs-popup';
 import '../styles/contact.css';
 
-export default () => (
-    <Popup
-        trigger={<button className='fill'>Connect With Our Team</button>}
-        modal
-        nested
-    >
-        {close => (
-            <div className="modal">
-                <button className="close" onClick={close}>&times;</button>
-                <div className="header"> We're happy to help.</div>
-                <div className="content">
-                    <form>
-                        <input name="name" type="text" class="feedback-input" placeholder="Name" />
-                        <input name="email" type="text" class="feedback-input" placeholder="Email" />
-                        <textarea name="text" class="feedback-input" placeholder="Comment"></textarea>
-                        <button className="send" onClick={() => {
-                        console.log('modal closed ');
-                        close();
-                    }}>Submit Your Question </button>
-                    </form>
-                </div>
-            </div>
-        )}
-    </Popup>
-);
+function Contact(){
+
+    function onClick()
+    {
+        window.location.href = `mailto:contact@greenvariable.com?subject=IT Consulting Request"`;
+    }
+
+    return (
+             <button onClick={onClick} className='fill'>Connect With Our Team</button>
+    )
+}
+
+export default Contact
